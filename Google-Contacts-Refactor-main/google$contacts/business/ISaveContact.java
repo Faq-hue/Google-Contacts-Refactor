13e034dbddebd55d8583dcb82a3ee658eeb8d3eb
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-05-13 18:57:53 -0300

    Delete Google-Contacts-Refactor-main directory

--- Google-Contacts-Refactor-main/google$contacts/business/ISaveContact.java
+++
@@ -1,7 +0,0 @@
-package business;
-
-import model.Contacts;
-
-public interface ISaveContact {
-  public void SaveContactInDisc(String directory,Contacts... contact);
-}
--- Google-Contacts-Refactor-main/google$contacts/business/ShowContact.java
+++
@@ -1,6 +0,0 @@
-package business;
-
-//TODO:code
-public class ShowContact {
-  
-}
