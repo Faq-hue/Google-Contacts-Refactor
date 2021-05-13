b4232ce38847a1e16e931e66e0110fe9f90e4a6f
Kevin Yanes <56029131+kyanesdev@users.noreply.github.com>
2021-05-13 17:47:53 -0300

    Update ShowContact.java

--- google$contacts/business/ShowContact.java
+++ google$contacts/business/ShowContact.java
@@ -1,6 +1,29 @@
 package business;
 
-//TODO:code
+import model.Contacts;
+import java.io.File;
+import java.util.Scanner;
+import com.google.gson.Gson;
+
 public class ShowContact {
-  
+    public class Listarcontactos {
+        public Contacts[] listsContacts(int cant) {
+            Gson g = new Gson();
+            Contacts[] contacts = new Contacts[5];
+            for (int i = 0; i < cant; i++) {
+                try {
+                    Scanner lectura = new Scanner(new File("datos" + File.separator + "conctacto" + i + ".json"));
+                    String datos = "";
+                    while (lectura.hasNextLine())
+                        datos = lectura.nextLine();
+                    contacts[i] = g.fromJson(datos, Contacts.class);
+                    lectura.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return contacts;
+        }
+    }
 }
+
