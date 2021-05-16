cfc39994eb026137e8e20c000549d6386599c601
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-05-16 14:58:38 -0300

    Add files via upload

---
+++ src/src/business/ISearchContact.java
@@ -0,0 +1,5 @@
+package src.business;
+
+public interface ISearchContact {
+  void searchAndShow(String directory,String key);
+}
---
+++ src/src/business/IShowContact.java
@@ -0,0 +1,5 @@
+package src.business;
+
+public interface IShowContact {
+  public void ShowContactInDisk(String directory);
+}
